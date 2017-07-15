2cf8ec0550e0590ee02e8089ef7ad5b1a111f14e
Keijiro Takahashi <keijiro@gmail.com>
2017-07-15 17:06:42 +0900

    Added waveform display.

--- Assets/Tester.cs
+++ Assets/Tester.cs
@@ -1,24 +1,58 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Tester : MonoBehaviour
 {
-    float[] waveform_;
+    [SerializeField] Transform _peakIndicator;
+    [SerializeField] Transform _rmsIndicator;
+    [SerializeField] Material _lineMaterial;
+
+    float[] _waveform;
+    List<Vector3> _vertices;
+    Mesh _mesh;
 
     void Start()
     {
+        _waveform = new float[512];
+
+        _mesh = new Mesh();
+        _mesh.MarkDynamic();
+
+        _vertices = new List<Vector3>(_waveform.Length);
+        for (var i = 0; i < _waveform.Length; i++) _vertices.Add(Vector3.zero);
+        _mesh.SetVertices(_vertices);
+
+        var indices = new int[2 * (_waveform.Length - 1)];
+        for (var i = 0; i < _waveform.Length - 1; i++)
+        {
+            indices[2 * i + 0] = i;
+            indices[2 * i + 1] = i + 1;
+        }
+        _mesh.SetIndices(indices, MeshTopology.Lines, 0);
+
         Lasp.PluginEntry.Initialize();
-        waveform_ = new float[512];
     }
 
     void OnDestroy()
     {
+        Destroy(_mesh);
         Lasp.PluginEntry.Terminate();
     }
 
     void Update()
     {
         var peak = Lasp.PluginEntry.GetPeakLevel(Time.deltaTime);
-        Lasp.PluginEntry.CopyWaveform(waveform_, waveform_.Length);
-        transform.localScale = Vector3.one * peak;
+        var rms = Lasp.PluginEntry.GetRmsLevel(Time.deltaTime);
+        Lasp.PluginEntry.CopyWaveform(_waveform, _waveform.Length);
+
+        _peakIndicator.localScale = new Vector3(1, peak, 1);
+        _rmsIndicator.localScale = new Vector3(1, rms, 1);
+
+        var scale = 2.0f / _waveform.Length;
+        for (var i = 0; i < _waveform.Length; i++)
+            _vertices[i] = new Vector3(scale * i - 1, _waveform[i], 0);
+        _mesh.SetVertices(_vertices);
+
+        Graphics.DrawMesh(_mesh, Vector3.zero, Quaternion.identity, _lineMaterial, gameObject.layer);
     }
 }
